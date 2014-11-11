827b117a7cb84cacdb29346188dbd8315ebe34cf
Johana Jaramillo <johana.jaramillo@mdeinteligente.co>
2014-11-11 15:01:55 -0500

    add user to module

--- Model/Accompaniment.php
+++ Model/Accompaniment.php
@@ -87,6 +87,16 @@ class Accompaniment extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+		'user_id' => array(
+				'notEmpty' => array(
+						'rule' => array('notEmpty'),
+						//'message' => 'Your custom message here',
+						//'allowEmpty' => false,
+						//'required' => false,
+						//'last' => false, // Stop validation after this rule
+						//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+		),
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
