faf6a16a7531de833a0acab2c4caa87d8e4d2f42
Johana Jaramillo <johana.jaramillo@mdeinteligente.co>
2015-03-09 16:08:55 -0500

    archivo js arreglado

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -48,7 +48,7 @@ App.prototype.bind=function(){
 App.prototype.removeRequired=function(){
 	$('.mde-form').find('.input.file').find('input').removeAttr('required');
 	$('.mde-form').find('.input.file').removeClass('required');
-	$('.mde-form').find('.input.file').find('input')removeClass('required');
+
 }
 
 App.prototype.bindAutocompletePersona=function(selector){
