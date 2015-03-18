907b792e55c3239a54f282002c9696e6e9b81c2f
unknown <nicolas.tobon@nicolas-tobon.meddigital.local>
2015-03-18 09:16:38 -0500

    Agregar colunda state en thematics sql

--- db/SQL STRUCTURE CHANGES.sql
+++ db/SQL STRUCTURE CHANGES.sql
@@ -6,3 +6,6 @@ ADD COLUMN `prefix` VARCHAR(45) NULL AFTER `name`;
 
 ALTER TABLE `appropriation_isp`.`divulgations` 
 ADD COLUMN `code` VARCHAR(45) NULL AFTER `id_divulgation`;
+
+ALTER TABLE `appropriation_isp`.`thematics` 
+ADD COLUMN `state` INT(11) NULL AFTER `name`;
