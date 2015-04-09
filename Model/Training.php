6b1b52eea83cc6c7c4dc640e11b6ba03bc07adea
unknown <yoddi.pino@yoddi-pino.meddigital.local>
2015-04-09 15:17:43 -0500

    se oculto el campo numero de participantes

--- Model/Training.php
+++ Model/Training.php
@@ -60,15 +60,10 @@ class Training extends AppModel {
 		),
 		'description' => array(
 			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				'rule' => array('notEmpty'),				
 			),
 		),
-		'participant_number' => array(
+		/*'participant_number' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
 				//'message' => 'Your custom message here',
@@ -77,7 +72,7 @@ class Training extends AppModel {
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
-		),
+		),*/
 		'type_id' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
