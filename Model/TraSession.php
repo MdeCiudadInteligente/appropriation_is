024a66475c3944f7a28fffdf8251590d0c459f56
unknown <nicolas.tobon@nicolas-tobon.meddigital.local>
2015-05-25 09:34:45 -0500

    All models changes ready

--- Model/TraSession.php
+++ Model/TraSession.php
@@ -47,6 +47,16 @@ class TraSession extends AppModel {
 							//'on' => 'create', // Limit validation to 'create' or 'update' operations
 					),
 		),	
+		'activity_place' => array(
+					'notEmpty' => array(
+							'rule' => array('notEmpty'),
+							//'message' => 'Your custom message here',
+							//'allowEmpty' => false,
+							//'required' => false,
+							//'last' => false, // Stop validation after this rule
+							//'on' => 'create', // Limit validation to 'create' or 'update' operations
+					),
+		),	
 		'training_id' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
--- Model/Training.php
+++ Model/Training.php
@@ -48,16 +48,6 @@ class Training extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'activity_place' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'description' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),				
