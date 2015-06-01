fd7c5f8bd6cb6152f134f99beaf105e95470934f
Johana Jaramillo <johana.jaramillo@mdeinteligente.co>
2015-06-01 15:53:23 -0500

    sql de exportar

--- db/Reportes/Acompañamientos - copia.sql
+++ db/Reportes/Acompañamientos - copia.sql
@@ -1,29 +1,25 @@
-SELECT 
+﻿SELECT 
     t1.accompaniment_date AS Fecha,
     'Acompañamiento' AS 'Tipo de actividad',
-    t5.name AS 'Tipo de acompañamiento',
+    t1.accompaniment_type AS 'Tipo de acompañamiento',
     t1.accompaniment_title AS 'Titulo',
     t1.accompaniment_description AS 'Descripcion',
-    t1.participant_number AS 'Numero de Participantes',
+    t1.participant_number AS 'Numero de Participantes',   
     t2.site_name AS Sitio,
     t3.neighborhood_name AS Barrio,
     t4.commune_name AS Comuna,
     t1.accompaniment_adjunct AS 'Adjunto 1',
     t1.accompaniment_adjunct1 AS 'Adjunto 2',
     t1.accompaniment_adjunct2 AS 'Adjunto 3'
-    FROM
+FROM
     accompaniments t1,
     sites t2,
     neighborhoods t3,
-    communes t4,
-    acc_types t5
-    
-    WHERE
+    communes t4
+WHERE
     t1.site_id = t2.id_site
-	AND t2.neighborhood_id = t3.id_neighborhood
-	AND t3.commune_id = t4.id_commune
-    AND t1.accompaniment_type_id = t5.id
-    AND t1.accompaniment_date BETWEEN '2015-04-1' AND '2015-04-30'
-    
-    ORDER BY 
-	t1.accompaniment_date,t5.name
+        AND t2.neighborhood_id = t3.id_neighborhood
+        AND t3.commune_id = t4.id_commune
+        AND t1.accompaniment_date BETWEEN '2015-04-1' AND '2015-04-30'
+ORDER BY 
+	t1.accompaniment_date,t1.accompaniment_type		
--- db/Reportes/Formaciones.sql
+++ db/Reportes/Formaciones.sql
@@ -1,6 +1,102 @@
 SELECT 
-				    t1.*,
-				    t2.name AS training_type,
+					t1.code AS Código,
+                    (SELECT 
+						zone_name
+						FROM
+						sites_trainings ta,
+						neighborhoods n,
+						communes c,
+						sites a,
+                        zones z                        
+						WHERE
+						t1.id = ta.training_id
+						AND ta.site_id = a.id_site
+						AND a.neighborhood_id = n.id_neighborhood 
+						AND n.commune_id= c.id_commune 
+                        AND c.zone_id= z.id_zone limit 1) AS Zonas,
+                        
+                        (SELECT 
+						commune_name
+						FROM
+						sites_trainings ta,
+						neighborhoods n,
+						communes c,
+						sites a
+						WHERE
+						t1.id = ta.training_id
+						AND ta.site_id = a.id_site
+						AND a.neighborhood_id = n.id_neighborhood 
+						AND n.commune_id= c.id_commune limit 1) AS Comunas ,
+                        
+                         (SELECT 
+						neighborhood_name
+						FROM
+						sites_trainings ta,
+						neighborhoods n,
+						sites a
+						WHERE
+						t1.id = ta.training_id
+						AND ta.site_id = a.id_site
+						AND a.neighborhood_id = n.id_neighborhood limit 1) AS Barrios , 
+                        
+                         (SELECT 
+				            GROUP_CONCAT(CONCAT(' ', site_name, ' '))
+				        FROM
+				            sites_trainings ta,
+				            sites a
+				        WHERE
+				            t1.id = ta.training_id
+				                AND ta.site_id = a.id_site) AS Sitios,
+                                
+						(SELECT 
+							GROUP_CONCAT(CONCAT(' ', site_type, ' '))
+						FROM
+							site_types st,
+							sites_trainings ta,
+							sites a
+						WHERE
+							t1.id = ta.training_id
+								AND ta.site_id = a.id_site
+								AND a.site_type_id = st.id_site_type
+						) AS Tipo_Sitio,
+                        
+                        (SELECT 
+							GROUP_CONCAT(CONCAT(' ', activity_place, ' '))
+						FROM
+							tra_session ts
+						WHERE
+							ts.training_id = t1.id) AS Lugar_Formación,
+							
+                    t2.name AS Tipo_Formación, 
+                    
+                    (SELECT 
+						GROUP_CONCAT(DISTINCT(CONCAT(' ', name, ' ')))
+					FROM
+						tra_session ts,
+						tra_sessions_thematics tst,
+						thematics th
+					WHERE
+						ts.training_id = t1.id
+						AND tst.session_id= ts.id
+						AND tst.thematic_id= th.id
+					) AS Temáticas,
+                    
+                     (SELECT 
+				            GROUP_CONCAT(CONCAT(' ', name, ' '))
+				        FROM
+				            population_types_training ta,
+				            population_types a
+				        WHERE
+				            t1.id = ta.training_id
+				                AND ta.population_type_id = a.id_population_type) AS Tipo_Población,
+                                
+                    t1.start_date AS Fecha_Inicio,
+                    t1.end_date AS Fecha_Final,
+					CASE t1.current_state
+                    WHEN t1.current_state='1' THEN 'En curso'
+                    ELSE 'Finalizado' 
+                    END AS Estado_Actual,
+				   
 				    (SELECT GROUP_CONCAT(CONCAT(' ', p.name, ' ', p.lastname, ' '))
 				        FROM
 				            training_per_trainers tPer,
@@ -11,7 +107,7 @@ SELECT
 				            t1.id = tPer.training_id
 				                AND tPer.per_trainer_id = per.id
 				                AND per.per_people_type_id = perTy.id
-				                AND perTy.person_id = p.id_person) AS formadores,				   
+				                AND perTy.person_id = p.id_person) AS Formadores,				   
 				    (SELECT 
 				            GROUP_CONCAT(CONCAT(' ', name, ' '))
 				        FROM
@@ -19,7 +115,7 @@ SELECT
 				            tra_allies a
 				        WHERE
 				            t1.id = ta.training_id
-				                AND ta.tra_ally_id = a.id) AS aliados,
+				                AND ta.tra_ally_id = a.id) AS Aliados,
 				    (SELECT 
 				            GROUP_CONCAT(CONCAT(' ', name, ' '))
 				        FROM
@@ -27,25 +123,8 @@ SELECT
 				            tra_processes a
 				        WHERE
 				            t1.id = ta.training_id
-				                AND ta.process_id = a.id) AS procesos,
-				    (SELECT 
-				            GROUP_CONCAT(CONCAT(' ', site_name, ' '))
-				        FROM
-				            sites_trainings ta,
-				            sites a
-				        WHERE
-				            t1.id = ta.training_id
-				                AND ta.site_id = a.id_site) AS sites,
-                                
-				    (SELECT 
-				            GROUP_CONCAT(CONCAT(' ', name, ' '))
-				        FROM
-				            population_types_training ta,
-				            population_types a
-				        WHERE
-				            t1.id = ta.training_id
-				                AND ta.population_type_id = a.id_population_type) AS poblacion,
-				    t3.username
+				                AND ta.process_id = a.id) AS Procesos,
+				    t3.username AS Usuario
 				FROM
 				    training t1,
 				    tra_types t2,
@@ -53,3 +132,5 @@ SELECT
 				WHERE
 				    t1.type_id = t2.id
 				        AND t3.id_user = t1.user_id
+                ORDER BY 
+				t1.code,t1.start_date      
