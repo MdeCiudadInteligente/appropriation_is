12facad3ceb184e91028f1bc7f2bb42cdab1a645
unknown <nicolas.tobon@nicolas-tobon.meddigital.local>
2015-07-13 10:07:02 -0500

    Release set

--- db/Release3/Release QUERYS/STEP1_STRUCT CHANGES.sql
+++ db/Release3/Release QUERYS/STEP1_STRUCT CHANGES.sql
@@ -621,8 +621,38 @@ ALTER TABLE `tra_session` ADD `activity_place` VARCHAR(200) NOT NULL AFTER `end_
 ALTER TABLE `training` DROP `activity_place`;
 
 
+
 ALTER TABLE `people` 
 CHANGE COLUMN `email` `email` VARCHAR(200) NULL ;
+
+
+
+CREATE TABLE IF NOT EXISTS `upload` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(500) NOT NULL,
+  `size` int(11) NOT NULL,
+  `ext` varchar(10) NOT NULL,
+  `tags` varchar(2000) DEFAULT NULL,
+  `meta` text,
+  `url` varchar(500) NOT NULL,
+  `session_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `creation_date` datetime NULL,
+  `modification_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre` (`name`),
+  KEY `session_id` (`session_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `upload`
+--
+ALTER TABLE `upload`
+  ADD CONSTRAINT `upload_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `tra_session` (`id`) ON UPDATE CASCADE;
 
 
 ALTER TABLE `people` ADD `document_type` INT NOT NULL AFTER `id_person`;
